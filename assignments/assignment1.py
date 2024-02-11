c1f16dc31f831421b7e0d51ea497d529f2b971fd
Alrik Fernandes <alrik.fernandes@gmail.com>
2024-02-11 14:35:50 -0800

    Added Odd and even point calculation

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -1,19 +1,20 @@
 import trimesh
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 
 class Vertice_Edge_Node:
-    def __init__(self,name, coordinates, incident_edge):
+    def __init__(self,name, coordinates, incident_edge=None):
         self.vname= name
         self.coordinates= coordinates
         self.incident_edge= incident_edge
 
     
     def __str__(self):  
-        return f" vertice : {self.vname},  coordinates: {self.coordinates}, incident: {self.incident_edge}"
+        return f"\n vertice : {self.vname},  coordinates: {self.coordinates}, incident: {self.incident_edge} |\n"
     
     def __repr__(self):  
-        return f" vertice : {self.vname},  coordinates: {self.coordinates}, incident: {self.incident_edge}"
+        return f"\n vertice : {self.vname},  coordinates: {self.coordinates}, incident: {self.incident_edge} |\n"
 
 class Face_Node:
     def __init__(self,name, half_edge):
@@ -21,10 +22,10 @@ class Face_Node:
         self.half_edge= half_edge
 
     def __str__(self):  
-        return f" face : {self.fname},  half edge: {self.half_edge}"
+        return f"\n face : {self.fname},  half edge: {self.half_edge} |\n"
     
     def __str__(self):  
-        return f" face : {self.fname},  half edge: {self.half_edge}"
+        return f"\n face : {self.fname},  half edge: {self.half_edge} |\n"
 
     
 
@@ -37,12 +38,15 @@ class Half_Edge_Node:
         self.incident_face = incident_face
         self.next = next
         self.prev = prev
+        self.child_odd_vertex = None
 
     def __str__(self):  
-        return f"\n half edge: {self.he_name}, origin vertice: {self.origin}, twin: {self.twin_name}, incident face: {self.incident_face}, next: {self.next}, prev: {self.prev } |\n"
+        return f"\n half edge: {self.he_name}, origin vertice: {self.origin}, end vertex: {self.end_vertex}, twin: {self.twin_name}, incident face: {self.incident_face}, next: {self.next}, prev: {self.prev }, mid: {self.child_odd_vertex} |\n"
     
     def __repr__(self):  
-        return f"\n half edge: {self.he_name}, origin vertice: {self.origin}, twin: {self.twin_name}, incident face: {self.incident_face}, next: {self.next}, prev: {self.prev } |\n"
+        return f"\n half edge: {self.he_name}, origin vertice: {self.origin}, end vertex: {self.end_vertex}, twin: {self.twin_name}, incident face: {self.incident_face}, next: {self.next}, prev: {self.prev }, mid: {self.child_odd_vertex} |\n"
+
+
 
 
 def create_half_edge(mesh, vertices_list ,half_edge_list, faces_list):
@@ -94,6 +98,84 @@ def create_half_edge(mesh, vertices_list ,half_edge_list, faces_list):
                     half_edge_list[twin_he_name].twin_name = he_name 
                     break
 
+def plot_half_edge(new_vertices_list):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Iterate through half edges and plot them
+    for e_name in half_edge_list:
+        org, end = half_edge_list[e_name].origin, half_edge_list[e_name].end_vertex
+        org_coords, end_coords = vertices_list[org].coordinates, vertices_list[end].coordinates
+        ax.plot([org_coords[0], end_coords[0]], [org_coords[1], end_coords[1]], [org_coords[2], end_coords[2]], color='b')
+
+    # Set labels and show plot
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+          
+
+    # Annotating vertices
+    for vertex_name in vertices_list:
+        ax.text(vertices_list[vertex_name].coordinates[0], vertices_list[vertex_name].coordinates[1], vertices_list[vertex_name].coordinates[2], vertex_name)
+
+        
+    for vertex_name in new_vertices_list:
+        ax.scatter(new_vertices_list[vertex_name].coordinates[0], new_vertices_list[vertex_name].coordinates[1], new_vertices_list[vertex_name].coordinates[2], color='r')
+        ax.text(new_vertices_list[vertex_name].coordinates[0], new_vertices_list[vertex_name].coordinates[1], new_vertices_list[vertex_name].coordinates[2], vertex_name)
+        
+
+    plt.show()
+
+def compute_odd_vertices(mesh,half_edge_list,vertices_list,new_vertices_list):
+    visited=set([])
+    for e_name in half_edge_list:
+        if e_name not in visited:
+            print(e_name,visited)
+            twin_e_name=half_edge_list[e_name].twin_name
+            visited.add(e_name)
+            visited.add(twin_e_name)
+
+            higher_weight_vertex1 = half_edge_list[e_name].origin
+            higher_weight_vertex2 = half_edge_list[e_name].end_vertex
+
+            lower_weight_vertex1 = half_edge_list[half_edge_list[e_name].next].end_vertex
+            lower_weight_vertex2 = half_edge_list[half_edge_list[twin_e_name].next].end_vertex
+
+            odd_vertex= 3/8*(vertices_list[higher_weight_vertex1].coordinates +vertices_list[higher_weight_vertex2].coordinates) \
+                       +1/8*(vertices_list[lower_weight_vertex1].coordinates + vertices_list[lower_weight_vertex2].coordinates)
+            
+
+            half_edge_list[e_name].child_odd_vertex = f"v{len(vertices_list)}"
+            half_edge_list[twin_e_name].child_odd_vertex = f"v{len(vertices_list)}"
+            new_vertices_list[f"v{len(new_vertices_list)}"] = Vertice_Edge_Node(f"v{len(new_vertices_list)}",np.array(odd_vertex))
+
+
+
+
+def compute_even_vertices(mesh,half_edge_list,vertices_list, new_vertices_list):
+    
+    for vertex_name in vertices_list:
+        neighbours = set([])
+        for he_name in half_edge_list:
+            if half_edge_list[he_name].origin == vertex_name:
+                neighbours.add(half_edge_list[he_name].end_vertex)
+        sum_neighbours = 0
+        for neighbour in neighbours:
+            sum_neighbours+= vertices_list[neighbour].coordinates
+        
+        print("Sum neighbours :", sum_neighbours)
+
+        beta=3/16.0
+        if len(neighbours)>3:
+            beta=3/(8*len(neighbours))
+        elif len(neighbours)<3:
+            raise Warning(f"Too few neighbours for {vertex_name} :: {neighbours}")
+
+        new_vertices_list[vertex_name].coordinates = vertices_list[vertex_name].coordinates*(1-len(neighbours)*beta) +sum_neighbours*beta
+
+
+
 
 def subdivision_loop(mesh, iterations=1):
     """
@@ -102,16 +184,26 @@ def subdivision_loop(mesh, iterations=1):
     :param iterations: number of iterations
     :return: mesh after subdivision
     """
-    mesh.show(flags={'wireframe': True,"vertices":True})
+    new_vertices_list = {}
+    new_half_edge_list = {}
+    for _ in range(iterations):
+        new_half_edge_list= copy.deepcopy(half_edge_list)
+        new_vertices_list = copy.deepcopy(vertices_list)
+        
+            # Step 1: Compute updated positions for existing vertices
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+        compute_odd_vertices(mesh,half_edge_list=half_edge_list,vertices_list=vertices_list,new_vertices_list=new_vertices_list )
+            # new_vertices.extend(midpoints)
 
 
-    ax.plot_trisurf(mesh.vertices[:, 0], mesh.vertices[:,1], mesh.vertices[:,2], triangles=mesh.faces);
+        compute_even_vertices(mesh,half_edge_list=half_edge_list,vertices_list=vertices_list,new_vertices_list=new_vertices_list)
+            # new_vertices.append(updated_position)
+    
+    plot_half_edge(new_vertices_list)
+    
 
-    plt.show()
-    return mesh
+    
+    return new_half_edge_list, new_vertices_list
 
 def simplify_quadric_error(mesh, face_count=1):
     """
@@ -152,15 +244,19 @@ if __name__ == '__main__':
 
     create_half_edge(mesh=mesh,vertices_list=vertices_list, half_edge_list=half_edge_list, faces_list=faces_list)
 
-    print(half_edge_list)
-    
-    
+    print("\n Half Edge List ::\n", half_edge_list)
 
-    print(vertices_list)
+    
+    print(" \n Faces List : ")
+
+    print("\n Vertices List \n",vertices_list)
+
+   
 
     # TODO: implement your own loop subdivision here
-    # mesh_subdivided = subdivision_loop(mesh, iterations=1)
+    new_half_edge_list, new_vertices_list = subdivision_loop(mesh, iterations=1)
     
+    print("\n New Vertices List \n",new_vertices_list)
    
    
    
